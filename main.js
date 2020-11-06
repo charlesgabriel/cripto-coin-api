00023054392acc226dc943af23b13bddee89e536
charlesgabriel <charlesgabriel@hotmail.com.br>
2020-11-06 17:00:06 -0300

    api coin market cap

---
+++ main.js
@@ -0,0 +1,25 @@
+var apikey = {
+  key: '[key Here]',
+}
+fetch('https://pro-api.coinmarketcap.com/v1/cryptocurrency/map?CMC_PRO_API_KEY=' + apikey.key)
+  .then((response) => {
+    if (!response.ok) throw new Error('Erro ao executar a requisição, status ' + response.status);
+    return response.json();
+  })
+  .then((api) => {
+    console.log("then api")
+    var texto = "";
+    for (let i = 0; i < 10; i++) {
+      texto = texto + `
+      <div class="media">
+        <img src="./coin.jpg" class="align-self-center mr-3" width="100" height="60" alt="coin">
+        <div class="media-body">
+          <h5 class="mt-2">${api.data[i].name}</h5>
+          <p>${api.data[i].symbol}</p>
+        </div>
+      </div>
+      `;
+      document.getElementById("coins").innerHTML = texto;
+    }
+  })
+  .catch((error) => { console.error(error.message); });
